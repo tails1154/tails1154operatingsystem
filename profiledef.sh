c7b1f6a191de661a7adfc096390ad16da839b861
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2026-07-12 20:02:22 -0500

    Remove vmlinuz from profiledef perms (outside valid path)

--- profiledef.sh
+++ profiledef.sh
@@ -21,7 +21,6 @@ file_permissions=(
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/boot/vmlinuz-linux"]="0:0:755"
   ["/usr/bin/trowser"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
